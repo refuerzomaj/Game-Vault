f776125304ca194c24ce1a9d01ad25ca30a26be0
Jam Gabitanan <refuerzomaj@gmail.com>
2022-03-21 11:46:42 +0800

    Third Commit

---
+++ Game-Vault/Pages/navbar.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="deals.php">Deals</a>
+        <a href="category.php">Category</a>
+        <a href="about.php">About</a>
+    </div>
+</header>
